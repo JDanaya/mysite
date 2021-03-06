d4ce1d2da6b17a83f5529e86f1cef3754d30986c
Jonhatan Anaya <jonhatan.anaya15@gmail.com>
2021-03-06 16:45:30 -0500

    Updates

---
+++ precache-manifest.b953526a1819a4bdae9f01d200b75671.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1ae031694b1e61b8a0af61a7753c94",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "2b8d9bb58a9947ce6454",
+    "url": "/mysite/static/css/main.a32b5059.chunk.css"
+  },
+  {
+    "revision": "1ac7d339ea0bee368edf",
+    "url": "/mysite/static/js/2.fed388ab.chunk.js"
+  },
+  {
+    "revision": "f7def617c168a3e9270f21ef8a75c08f",
+    "url": "/mysite/static/js/2.fed388ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8d9bb58a9947ce6454",
+    "url": "/mysite/static/js/main.96bbcf78.chunk.js"
+  },
+  {
+    "revision": "04757bf71514d23d0473",
+    "url": "/mysite/static/js/runtime-main.8c9947cd.js"
+  },
+  {
+    "revision": "ff31f6d2c0965fc630a3930f4bb9750f",
+    "url": "/mysite/static/media/8401.ff31f6d2.jpg"
+  },
+  {
+    "revision": "ffdf345a0ba2ae1f192ee2134f2b3de9",
+    "url": "/mysite/static/media/imgp.ffdf345a.png"
+  },
+  {
+    "revision": "9bc995b7f0f167ccde52f063f3f8d01a",
+    "url": "/mysite/static/media/reven.9bc995b7.png"
+  }
+]);
